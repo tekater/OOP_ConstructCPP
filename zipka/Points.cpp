750eab42756eabc6bd9a41e32914d249ddb89b44
tekater <120014627+tekater@users.noreply.github.com>
2023-05-15 21:15:53 +0900

    Create Points.cpp

---
+++ zipka/Points.cpp
@@ -0,0 +1,16 @@
+#include "Points.h"
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <math.h>
+
+using namespace std;
+
+
+
+void Point::print(const Point& p) {
+	cout << " ";
+}
+
+
