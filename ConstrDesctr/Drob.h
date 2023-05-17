e113a3f5c79443d425bd24fc253bd20e0aa7d968
tekater <120014627+tekater@users.noreply.github.com>
2023-05-17 19:40:28 +0900

    Create Drob.h

---
+++ ConstrDesctr/Drob.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Drob {
+    int ch, zn;
+public:
+    Drob() {
+        ch = 1;
+        zn = 10;
+        cout << "Дефолт Конструктор\n";
+    }
+    Drob(int x) {
+        ch = x;
+        zn = x;
+        cout << "1 int Конструктор\n";
+    }
+    Drob(int x, int y) {
+        ch = y;
+        zn = x;
+        cout << "2 int Конструктор\n";
+    }
+    void print();
+
+    ~Drob() {
+        cout << "\nДеструктор отработан!!!.\n";
+    }
+};
